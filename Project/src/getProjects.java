521e585b3a56bdc8e155b164d17898c706da93b5
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-05-05 18:30:18 +0530

    Upadted Files

--- Project/src/getProjects.java
+++
@@ -1,9 +0,0 @@
-
-
-public class getProjects {
-	public static RestResponse getProjects() {
-        String body = "{\"kind\":\"project\",\"length\":100,\"offset\":0}";
-       RestApi obj = RestApi.getInstance();
-       return obj.restCall("projects/list", body, "POST");
-}
-}
