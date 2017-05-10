49fcd112058020f1273a748fc4c9c0ba74acfe1b
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-05-10 14:54:31 +0530

    JavaClientApplication

---
+++ JavaApplicationStatusCode/src/sam/Product.java
@@ -0,0 +1,15 @@
+package sam;
+
+public class Product {
+
+	public void setName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setQty(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
