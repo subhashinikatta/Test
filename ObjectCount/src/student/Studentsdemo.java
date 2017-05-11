9ef4f2637c389456b41c9c78c5ba95d24f64087d
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-05-11 15:19:57 +0530

    Added ObjectCount

--- ObjectCount/src/student/Studentsdemo.java
+++ ObjectCount/src/student/Studentsdemo.java
@@ -4,10 +4,10 @@ class Studentsdemo
     
 public static void main(String[]args)
 {
-Students student1= new Students("Michelle", 7);
-Students student2= new Students("Daniel", 8);
-Students student3= new Students("Vanessa", 9);
-Students student4= new Students("Ryan", 8);
+Students student1= new Students("Subbu", 7);
+Students student2= new Students("Vasu", 8);
+Students student3= new Students("Ravi", 9);
+Students student4= new Students("Vicky", 8);
 
 
 System.out.println ("There are " + Students.numberofobjects + " objects in this class");
