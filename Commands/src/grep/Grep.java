3b221c6143a633b9d2dc9d4d1cc6bdc12bc9b1bf
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-05-12 15:37:25 +0530

    Grep Added

---
+++ Commands/src/grep/Grep.java
@@ -0,0 +1,42 @@
+
+import java.util.regex.*;
+import java.io.*;
+
+class Grep {
+    public static void main(String args[]) {
+        if (args.length != 2) {
+            System.err.println("Usage: Grep pattern file");
+            System.exit(1);
+        }
+
+        Pattern cre = null;        // Compiled RE
+        try {
+            cre = Pattern.compile(args[0]);
+        } catch (PatternSyntaxException e) {
+            System.err.println("Invalid RE syntax: " + e.getDescription());
+            System.exit(1);
+        }
+
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(
+                 new FileInputStream(args[1])));
+        } catch (FileNotFoundException e) {
+            System.err.println("Unable to open file " +
+                args[1] + ": " + e.getMessage());
+            System.exit(1);
+        }
+
+        try {
+            String s;
+            while ((s = in.readLine()) != null) {
+                Matcher m = cre.matcher(s);
+                if (m.find())
+                    System.out.println(s);
+            }
+        } catch (Exception e) {
+            System.err.println("Error reading line: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
