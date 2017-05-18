5110728da531b3db345b405af636d4625823e22c
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-05-18 15:59:12 +0530

    Added Static method in Tail

--- Commands/src/tail/Tail1.java
+++ Commands/src/tail/Tail1.java
@@ -1,42 +1,53 @@
-package tail;  
+package tail;
 import java.io.BufferedReader;
-    import java.io.FileReader;
-    import java.io.IOException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-    
-    public class Tail1 {
+public class Tail1{
 
-      static long sleepTime = 1000;
+private static String fileName="";
+public static int tailSize;
 
-      public static void main(String[] args) throws IOException {
 
-        if (args.length > 0){
+public static StringBuffer tail(){
+String thisLine;
+StringBuffer output = new StringBuffer("");
+ArrayList<String> List = new ArrayList<String>();
+try{
+BufferedReader br = new BufferedReader(new FileReader(fileName));
+while( ( thisLine = br.readLine() ) != null )
+{
+List.add( thisLine );
+}
 
-          if (args.length > 1)
-        sleepTime = Long.parseLong(args[1]) * 1000;
+} // end try
+catch( IOException e )
+{
+output.append( "Error reading file: " + e );
+}
+int end= List.size();
+int start = List.size() - tailSize;
+if(start<0)
+{
+start =0;
+tailSize= end;
+}
 
-          BufferedReader input = new BufferedReader(new FileReader(args[0]));
-          String currentLine = null;
+for(int i=start; i <end; i++)
+{
+output.append( (String)List.get(i) + "\n" );
+}
+return output;
+}
+public static void main(String args[]){
+int argsLength = args.length;
 
-          while (true) {
+if( argsLength == 0 ) System.exit(0);
+if( argsLength == 2 )
+{
 
-        if ((currentLine = input.readLine()) != null) {
-          System.out.println(currentLine);
-          continue;
-        }
-
-        try {
-          Thread.sleep(sleepTime);
-        } catch (InterruptedException e) {
-          Thread.currentThread().interrupt();
-          break;
-        }
-
-          }
-          input.close();
-
-        } else {
-          System.out.println("Missing parameter!\nUsage: java JavaTail fileName [updateTime (Seconds. default to 1 second)]");
-        }
-      }
-    }
+System.out.println(Tail1.tail() );
+}
+}
+}
