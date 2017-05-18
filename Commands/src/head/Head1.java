ac1e625fe90a4e48275d688e0b463242c30d626b
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-05-18 17:44:52 +0530

    Added Head using Static method

---
+++ Commands/src/head/Head1.java
@@ -0,0 +1,36 @@
+package head;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Head1 {
+    public static String[] headLines(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        int n = 0;
+        String[] lines = new String[n];
+        String line;
+       
+        int Line = 0;
+        while((line = reader.readLine()) != null) {
+            lines[Line++] = line;
+            if(Line >= n) {
+                break;
+            }
+        }
+        return lines;
+    }
+
+    public static void main(String[] args) throws IOException {
+        if(args.length >= 1) {
+            System.out.println("Error");
+        }
+        String[] lines = headLines(args[0]);
+        for(String line : lines) {
+            if(!line.isEmpty()) {
+                System.out.println(line);
+            }
+        }
+    }
+}
