d21ac6ff68f84b47361e50a153f01aaf7f4f0b32
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-05-18 12:38:08 +0530

    Added Tail

--- Commands/src/tail/Tail.java
+++ Commands/src/tail/Tail.java
@@ -1,83 +1,58 @@
 package tail;
-
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Vector;
+import java.util.ArrayList;
 
-public class Tail {
-	
-//tail method starts
-     public static  StringBuffer Tail(int tailSize, String filename) throws IOException
-         {
-    	
-    	 Vector<String> List = new Vector<String>(5000,500);
-    	 StringBuffer output = new StringBuffer("");
-    	 String thisLine;
+public class Tail{
 
-    	 try
-    	 {
-    	 FileReader fr = new FileReader( filename );
-    	 BufferedReader myInput = new BufferedReader( fr );
+private String fileName="";
+public int tailSize;
 
-    	 while( ( thisLine = myInput.readLine() ) != null )
-    	 {
-    	 List.add( thisLine );
-    	 }
+ArrayList<String> List = new ArrayList<String>();
+public Tail(int tailSize , String fileName)
+{
+this.fileName=fileName;
+this.tailSize= tailSize;
+}
+public StringBuffer  run(){
+String thisLine;
+StringBuffer output = new StringBuffer("");
+try{
+FileReader fr = new FileReader( fileName );
+BufferedReader br = new BufferedReader( fr );
+while( ( thisLine = br.readLine() ) != null )
+{
+List.add( thisLine );
+}
 
-    	 } // end try
-    	 catch( IOException e )
-    	 {
-    	 output.append( "Error reading file: " + e );
-    	 }
+} // end try
+catch( IOException e )
+{
+output.append( "Error reading file: " + e );
+}
+int end= List.size();
+int start = List.size() - tailSize;
+if(start<0)
+{
+start =0;
+tailSize= end;
+}
 
-    	 List.trimToSize();
+for(int i=start; i <end; i++)
+{
+output.append( (String)List.get(i) + "\n" );
+}
+return output;
+}
+public static void main(String args[]){
+int argsLength = args.length;
 
-    	 int end = List.size();
-    	 int start = List.size() - tailSize;
-    	 if( start < 0 )
-    	 { start = 0;
-    	 tailSize = end; 
-    	 }
-    	 output.append( "========== Tail [" + filename + "]\n" );
-    	 output.append( "========== Showing last [" + tailSize + "] lines\n" );
-
-    	 for( int i = start; i < end; i++ )
-    	 {
-    	 output.append( (String)List.get(i) + "\n" );
-    	 }
-    	 output.append( "========== END\n" );
-
-    	 return output;
-    	 }
-     public static void main(String args[])
-     {
-       
-         String filename="";
-         int tailSize = 0;
-         if (args.length==2)
-         {
-        	 filename=args[1];
-         }
-         else
-         {
-             exit();
-         }
-          
-         try
-         {
-        	System.out.println(Tail(tailSize, filename));  // calling tail method to search lines
-         }
-         catch(IOException io)  { 
-        	 System.out.println("IO Error"); 
-        	 }
-     }
-
-     public static void exit()
-     {
-             System.out.println("Syntax : java tail [options] file_name "  );
-            
-             System.exit(0);
-     }
+if( argsLength == 0 ) System.exit(0);
+if( argsLength == 2 )
+{
+Tail tail = new Tail( new Integer( args[1] ).intValue(), args[0]);
+System.out.println( tail.run().toString() );
+}
+}
 }
