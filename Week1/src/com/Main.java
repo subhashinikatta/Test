9bebe9ebf68728f2948b20be879f7543531cdf8a
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-05-26 12:04:58 +0530

    Final

--- Week1/src/com/Main.java
+++ Week1/src/com/Main.java
@@ -39,6 +39,15 @@ public class Main {
                      Demo.contains(args[0],args[1]);
                      
               }
+              if(args.length == 3){
+            	  Demo.quadraticEquation(Integer.parseInt(args[0]),Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+            	 Demo.modifyFile(args[0],args[1],args[2]);
+            	Demo.replaceString(args[0],args[1],args[2]);
+              }
+              
+              if(args.length >= 4){
+            	  Demo.evenOddPrime(args);
+              }
 }
 }
               
