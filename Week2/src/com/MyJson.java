58f4cf78caa19fdfd898ad79578c304b723b5e89
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-06-05 09:36:17 +0530

    JsonFile

--- Week2/src/com/MyJson.java
+++ Week2/src/com/MyJson.java
@@ -1,50 +1,41 @@
 package com;
 
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
 
-import org.json.JSONException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 public class MyJson {
-	public static Map<String,String> getKeyValue(JSONObject json , Map<String,String> out) throws JSONException{
-		json = new JSONObject();
-		 Set keys = json.keySet();
-		    Iterator a = keys.iterator();
-		    while(a.hasNext()) {
-		    	String key = (String)a.next();
-		        JSONObject value = (JSONObject)json.get(key);
-		        getKeyValue(value,out);
-		    }
-		  
-		    return out;
-	}
-	 public static void main(String[] args) throws JSONException, IOException, ParseException {
+
 	
-		 JSONParser parser = new JSONParser();
+	static String getKeyValue(JSONObject jsonobj , String key){
+		try {
+			JSONObject jsonObject=jsonobj;
+			System.out.println(jsonObject.get(key));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return key;
 
-		 FileReader fileReader = new FileReader("C:/java/Week1/petstore.json"); 
-		 JSONObject json = (JSONObject) parser.parse(fileReader);
+	}
 
-	    Map<String,String> out = new HashMap<String, String>();
-	  
-	    JSONObject jsonObject = new JSONObject           (json);
-	    JSONObject info     = (JSONObject) jsonObject.get ("info");
-	    JSONObject paths   = (JSONObject) info.get("paths");
-	    System.out.println("paths: " + jsonObject.get("paths"));
-	    System.out.println("definitions: " + jsonObject.get("definitions"));
-	    JSONObject definitions   = (JSONObject) info.get ("definitions");
-	    getKeyValue(jsonObject,out);
+	static String getPath(JSONObject jsonobj , String key){
+		try {
+			JSONObject obj = (JSONObject) jsonobj.get(key);
+			System.out.println(obj.keySet());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return key;
+
+	}
+
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+		JSONParser parser = new JSONParser();
+		JSONObject jsonobj=  (JSONObject) parser.parse(new FileReader("C:/Users/kshubhashini/workspace3/Week2/src/com/petstore.json"));
+		getKeyValue(jsonobj,"paths");
+		getPath(jsonobj,"paths");
 	}
 }
-
-
-
-
-
