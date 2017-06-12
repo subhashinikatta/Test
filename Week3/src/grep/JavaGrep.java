998fe2490ae4a19174a91286064ec8d0b3653c15
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-06-12 15:30:58 +0530

    Change Grep

--- Week3/src/grep/JavaGrep.java
+++ Week3/src/grep/JavaGrep.java
@@ -1,183 +1,136 @@
 package grep;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+class JavaGrep
+{
+    public static void main(String args[])
+        {
+            String searchWord="";
+            String fileName="";
+            String options="";
+            String optionsAllowed="vnicwe";//Options allowed in the input
+            if (args.length==2)
+            {
+                searchWord=args[0];
+                fileName=args[1];
+            }
+            else if (args.length==3)
+              {
+                options=args[0];    //options may contain  vnic.   
+                searchWord=args[1];
+                fileName=args[2];
+            }
+            else
+            {
+                exit();
+            }
+             
+             
+            if(options.length()>4 )   // length  of options may not be more than 4 chars. 
+                {
+                    exit();   
+                }
+             
+                char OA[]=options.toCharArray(); //Options converted to char array
+                 
+            for (int i=0 ; i<OA.length;i++)     
+                if( optionsAllowed.indexOf(OA[i])==-1)  //if options contains other than vnci, it exits
+                { 
+                    System.out.println("Invalid options");
+                    exit();
+                }
+                     
+             
+            try
+            {
+                grep(options, searchWord, fileName);  // calling grep method to search lines
+            }
+            catch(IOException io)  { System.out.println("IO Error");  }
+        }
+// grep method starts
+        public static void grep(String options, String exp, String filename) throws IOException
+            {
+                int count = 0;
+                int flag=0;
+                int lineNo=0;
+                int countNM=0;
+                // open the file
+                BufferedReader brdr = new BufferedReader(new FileReader(filename));
+                String line;
+                if(options.contains("i"))  flag=2;// for case insensitive
+                    Pattern pattern=null;
+                    // reading  each line
+                    while( (line = brdr.readLine( )) != null)
+                    {
+                          lineNo++;  
+                          try
+                           {
+                              pattern = Pattern.compile(exp, flag); //flag 2 to mention case insensitive
+                           }
+                          catch (PatternSyntaxException e)    { System.out.println("Error");   } 
+                     
+                          Matcher matcher = pattern.matcher(line);
+                         if (matcher.find())
+                          {
+                             count++;    //counting matching lines
+                            //printing only matching lines
+                            if (!options.contains("v"))
+                            {
+                                if (options.contains("n")) 
+                                    System.out.println(lineNo + " : " + line);  
+                                else System.out.println(line);
+                            }
+                          }
+                         else
+                            {
+                             countNM++;  // counting not matching lines
+                             //printing not matching lines
+                            if (options.contains("v"))   
+                            {
+                                if (options.contains("-n"))  
+                                    System.out.println(lineNo + " : " + line);  
+                                else
+                                    System.out.println(line); 
+                            }
+                            }
+                         if(options.contains("-w"))//match only whole words 
+                         {
+        						if (matcher.find())
+        						{
+        							if(line.equals(exp)){
 
-public class JavaGrep {
+        								System.out.println(line);
+        							}
+        						}
+                            }
+                         if(options.contains("e"))//use regex PATTERN for matching
+                         {
+ 							if (matcher.lookingAt()) {
+ 								if (filename != null) {
 
-		public static void main(String args[])
-		{ 
-			String matchWord="";
-			String fileName="";
-			String options="";
-			String optionsAllowed="-i –e –v –w –c -n";//grep options
-			if (args.length==2)
-			{
-				matchWord=args[0];
-				fileName=args[1];
-			}
-			else if (args.length==3)
-			{
-				options=args[0];    //options may contain  -i –e –v –w –c -n.
-				matchWord=args[1];
-				fileName=args[2];
-			}
-			else
-			{
-				exit();
-			}
-
-
-			if(options.length()>2)  // option is more than two allowed
-			{
-				exit();
-			}
-
-			char array[]=options.toCharArray(); 
-
-			for (int i=0 ; i<array.length;i++)
-				if( optionsAllowed.indexOf(array[i])==-1)  //if options contains other than -i –e –v –w –c -n, it exits
-				{
-					System.out.println("Invalid options");
-					exit();
-				}
-			try
-			{
-				grep(options, matchWord, fileName);  // calling grep method to search lines
-			}
-			catch(IOException io)  { System.out.println("IOerror");  }
-		}
-
-
-		// grep method starts
-		public static void grep(String options, String exp, String fileName) throws IOException
-		{
-
-			int count = 0;
-			int lineNo=0;
-			int countNM=0;
-			int flag=0;
-			String Line = null;
-			Pattern pattern=null;
-			try {
-				FileReader filereader = new FileReader( fileName );
-				BufferedReader brdr = new BufferedReader( filereader );
-				while( true ) {// open the file
-					switch (options) {
-					case "-i":// for case insensitive
-                       flag=2;// for case insensitive(default)
-						while( (Line = brdr.readLine( )) != null) // reading  each line
-						{
-							pattern=Pattern.compile(exp,flag);// to mention case insensitive
-							Matcher matcher = pattern.matcher(Line);
-							if (matcher.find())
-							{
-								count++;    //counting and printing matching lines
-								
-								System.out.println(Line);
-
-							}
-						}
-						break;
-					case "-v":// select non-matching lines
-						while( (Line = brdr.readLine( )) != null)
-						{
-
-							pattern=Pattern.compile(exp);
-							Matcher matcher = pattern.matcher(Line);
-							if (matcher.find()){}
-							else{
-								System.out.println(Line);
-							}
-						}
-
-						break;
-
-					case "-n":// print line number with output lines
-						while( (Line = brdr.readLine( )) != null)
-						{
-							lineNo++;
-							pattern=Pattern.compile(exp);
-
-							Matcher matcher = pattern.matcher(Line);
-							if (matcher.find())
-							{
-
-								System.out.println(lineNo + " : " + Line);
-							}
-
-						}
-						break;
-					case "-c":// print NUM lines of output context
-
-						while( (Line = brdr.readLine( )) != null)
-						{
-							pattern = Pattern.compile(exp); 
-							Matcher matcher = pattern.matcher(Line);
-							if (matcher.find())
-							{
-								count++;    //counting and printing matching lines
-								
-							}else 
-							{
-								countNM++;  // counting and printing not matching lines
-								
-							}
-						}
-						System.out.println("\n Word / Exp : " + exp );
-						System.out.println(count + " line(s) matched.");
-						System.out.println(countNM + " line(s) not matched.");
-						break;
-						
-					case "-e"://use regex PATTERN for matching
-						
-						while( ( Line = brdr.readLine() ) != null ) {
-							pattern = Pattern.compile(exp); 
-							Matcher matcher = pattern.matcher(Line);
-							if (matcher.lookingAt()) {
-								if (fileName != null) {
-
-								}
-								System.out.println(Line);
-							}
-						}
-						break;
-						
-					case "-w"://match only whole words 
-						
-						while( (Line = brdr.readLine( )) != null) // reading  each line
-						{
-							pattern=Pattern.compile(exp);
-							Matcher matcher = pattern.matcher(Line);
-
-
-							if (matcher.find())
-							{
-								if(Line.equals(exp)){
-
-									System.out.println(Line);
-								}
-							}
-						}
-						break;
-						
-					default:
-						exit();
-					}
-
-					brdr.close();
-				}
-		} catch (Exception e) { System.out.println(""); }
-		}
-		
-		public static void exit()
-		{
-			System.out.println("Syntax : java JavaGrep [options] regular_expression/word file_name "  );
-			System.out.println("Options Allowed : -i –e –v –w –c -n "  );
-			System.exit(0);
-		}
-	}  
-
+ 								}
+ 								System.out.println(line);
+ 							}
+ 						}
+                    }
+                        //Lines count for both matching  &  not matching 
+                        if (options.contains("c"))  
+                        {
+                            System.out.println("\n Word / Exp : " + exp );
+                            System.out.println(count + " line(s) matched.");
+                            System.out.println(countNM + " line(s) not matched.");
+                        }
+                       
+                          
+                        brdr.close( );
+                }
+ 
+        public static void exit()
+        {
+                System.out.println("Syntax : java JavaGrep [options] regular_expression/word file_name "  );
+                System.out.println("Options Allowed : i or n or c or v or e or w  or any combinations "  );
+                System.exit(0);
+        }
+}
