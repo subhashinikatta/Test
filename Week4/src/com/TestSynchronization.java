040daea4e0e512c066b178f6f0514042055cbac5
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-06-20 09:47:28 +0530

    Week4 Assignment

--- Week4/src/com/TestSynchronization.java
+++ Week4/src/com/TestSynchronization.java
@@ -3,8 +3,11 @@ package com;
 
 public class TestSynchronization
 {  
+	static Object Lock1 = new Object();
+	   static Object Lock2 = new Object();
 public static void main(String args[])
 {  
+	
 ThreadFileWriter obj = new ThreadFileWriter();//only one object  
 MyThread1 t1=new MyThread1(obj);  
 MyThread2 t2=new MyThread2(obj); 
@@ -13,4 +16,51 @@ t2.setName("vasu");
 t1.start();  
 t2.start();
 } 
+private static class MyThread1 extends Thread {
+	private ThreadFileWriter t;
+
+	MyThread1(ThreadFileWriter t)
+	{  
+	this.t=t;  
+	}  
+
+	public void run() {
+       synchronized (Lock1) {
+    	   ThreadFileWriter.fileWrite("subbu.txt","hiiiiii");  
+          System.out.println("Thread 1: Holding lock 1...");
+          
+          try {
+             Thread.sleep(10);
+          }catch (InterruptedException e) {}
+          System.out.println("Thread 1: Waiting for lock 2...");
+          
+          synchronized (Lock2) {
+             System.out.println("Thread 1: Holding lock 1 & 2...");
+          }
+       }
+    }
+ }
+private static class MyThread2 extends Thread {
+	private ThreadFileWriter t;
+
+	MyThread2(ThreadFileWriter t)
+	{  
+	this.t=t;  
+	}  
+    public void run() {
+       synchronized (Lock2) {
+    	   ThreadFileWriter.fileWrite("subbu1.txt","hiiiiii");  
+          System.out.println("Thread 2: Holding lock 1...");
+         
+          try {
+             Thread.sleep(10);
+          }catch (InterruptedException e) {}
+          System.out.println("Thread 2: Waiting for lock 2...");
+          
+          synchronized (Lock1) {
+             System.out.println("Thread 2: Holding lock 1 & 2...");
+          }
+       }
+    }
+ } 
 }
