8cee10049ca271ce02cedf64b456c087c739a2e8
subhashinikatta <kshubhahsini-scholar@hcl.com>
2017-06-20 17:42:34 +0530

    Change UserServices

--- JerseyUser/src/webservice/UserService.java
+++ JerseyUser/src/webservice/UserService.java
@@ -13,6 +13,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.omg.CORBA.portable.ApplicationException;
 
 import com.google.gson.Gson;
 import com.mysql.jdbc.Connection;
@@ -31,256 +34,93 @@ public class UserService {
 	
 	  UserDAO userDao = new UserDAOImplementation();
 		
-@POST
- @Path("/login")
-/*@Produces(MediaType.APPLICATION_JSON) */
-/* @Consumes(MediaType.APPLICATION_FORM_URLENCODED) */
- public String login(@FormParam("username") String username,
- @FormParam("password") String password) {
- 
-	return getAllUsersList(username, password);
-	 
-	}
-	 
-	public String getAllUsersList(String username,String password)
-	 {
-	 String userListData = null;
-	 try 
-	 {
-	 ArrayList<User> userList = null;
-	 SecurityManager securityManager= new SecurityManager();
-	 userList = securityManager.getAllUsersList();
-	 for (User user : userList) {
-	 if(user.getusername().equals(username))
-	 {
-	 if(user.getpassword().equals(password))
-	 {
-	 return "Logged in User:"+username;
-	 }
-	 }
-	 }
-	 
-	} catch (Exception e)
-	 {
-	 System.out.println("error");
-	 }
-	 return "Logged in User:"+username;
-	 }
-@POST
-@Path("/register")
-@Produces(MediaType.APPLICATION_JSON) 
 
-public String register(@FormParam("FirstName") String FirstName,
-		@FormParam("LastName") String LastName,
-		@FormParam("Email") String Email,
-        @FormParam("username") String username,
-        @FormParam("password") String password,
-        @FormParam("confirmpassword") String confirmpassword){
-	
-	 User user = new User(FirstName,LastName,Email,username,password,confirmpassword);
-   
-	
-	 userDao.addUser(user);
-	
-     String users=null;
-     ArrayList<User> userList = new ArrayList<User>();
-     try
- {
-	 
-	 SecurityManager securityManager= new SecurityManager();
-	 userList = securityManager.getAllUsersList();
-	
-	// for (User user1 : userList) {
-		 String EMAIL_PATTERN = 
-				 "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-				 + "+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-				 Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-if( user.getEmail() == null || user.getEmail().trim().equals("")&& !pattern.matcher(Email).matches()&& user.getusername() == null || user.getusername().trim().equals("")&& user.getFirstName() == null || user.getFirstName().trim().equals("")&& user.getLastName() == null || user.getLastName().trim().equals(""))
-{
-					  
-						  //System.out.println("please enter valid details");
-					  }
-/*if(FirstName.isEmpty()||LastName.isEmpty()||Email.isEmpty()||username.isEmpty()||password.isEmpty()||confirmpassword.isEmpty())
-	{
-			 System.out.println("Please fill all the fields");
- 	}
-	String emailRegex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-			 + "+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-	 Pattern pattern = Pattern.compile(emailRegex);
-	   
-		 if( FirstName == "" )
-		   {
-			 System.out.println("Please enter the first names");
-		     return FirstName;
-		   }
-		 if( LastName == "" )
-		   {
-		     
-			 System.out.println("enter the last name");
-		     return LastName;
-		   }
-		    
-		   if (Email == "" )
-		 {
-			   System.out.println( "enter the email");
-		  return Email;
-		  }else if(!emailRegex.test(Email)){
-		  
-			  System.out.println( "enter the valid email");
-		  return false;
-		  }
-		   
-			 if(username == "")
-			  {
-			  
-				 System.out.println( "enter the username");
-			   return username;
-			  } 
-		  
-		   
-		 if(password == "")
-		  {
-		  
-			 System.out.println( "enter the password");
-		   return password;
-		  }
-		  
-		 if(confirmpassword == "")
-		  {
-		  
-			 System.out.println( "enter the confirmpassword");
-		   return confirmpassword;
-		  }else if(!emailRegex.test(confirmpassword)){
-			  System.out.println("Re-enter the valid password");
-			  return confirmpassword;
-			  }
+
+		@POST
+		@Path("/log")
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response logInJSON(User user) throws SQLException {
+			 {
+
+				 String userListData = null;
+
+				 ArrayList<User> userList = new ArrayList<User>();
+
+				 try 
+
+				 {
+
+				 
+
+				 SecurityManager securityManager= new SecurityManager();
+
+				 userList = securityManager.getAllUsersList();
+
+				
+				 {
+
+				 System.out.println( "Logged in User");
+
 			
-		  
-		 
-		  if(FirstName != "" && LastName != "" && Email != "" && username != "" && password != "" && confirmpassword != "" ){
-			  System.out.println( "form submitted successfully");
-		   }
-		     */
-	
 
-else{
-	userList.add(user);
-	Gson gson=new Gson();
-	users =gson.toJson(user);
-}
-	 }
-	 
-	 catch (Exception e)
-	 {
-	 System.out.println("error");
-	 }
-	 
-    /* if(result != 1){ 
-       return "Invalid Details"; 
-    	 } */
-    	      return "successfully Registered values"+users ; 
-    	     
- } 
-@PUT
-@Path("/update")
-@Produces(MediaType.APPLICATION_JSON) 
-@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-public String update(@FormParam("FirstName") String FirstName,
-		@FormParam("LastName") String LastName,
-		@FormParam("Email") String Email,
-        @FormParam("username") String username,
-       @FormParam("password") String password,
-       @FormParam("confirmpassword") String confirmpassword){
-	
-	User user = new User(FirstName,LastName,Email,username,password,confirmpassword);
-	 userDao.updateUser(user);
-	 
-     String users=null;
-	 try 
-	 {
-	 ArrayList<User> userList = null;
-	 SecurityManager securityManager= new SecurityManager();
-	 userList = securityManager.getAllUsersList();
-	// for (User user2 : userList) {
-		 String EMAIL_PATTERN = 
-				 "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-				 + "+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-				 Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-if( user.getEmail() == null || user.getEmail().trim().equals("")&& !pattern.matcher(Email).matches())
-{
-	//System.out.println("please enter valid email");
-		 
-}
-else{
-	if(user.getEmail().equals(Email))	{			  
-		userList.add(user);
+				 }
+
+				 
+
+				} catch (Exception e)
+
+				 {
+
+				 System.out.println("error");
+
+				 }
+
+				 System.out.println("You are not a Valid User");
+
+				 }
+			String result = " Login : " + user;
 		
-		Gson gson =new Gson(); 
-	  users =gson.toJson(user); 
-	  } 
+			return Response.status(201).entity(result).build();
+			
+		}
+		@POST
+		@Path("/post")
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response regInJSON(User user) throws SQLException {
+			
+			userDao.addUser(user);
+			  ArrayList<User> userList = new ArrayList<User>();
+			  
+			 	userList.add(user);
+			String result = " saved : " + user;
+		
+			return Response.status(201).entity(result).build();
+			
+		}
 
-}
-	 
-	 }
-	 catch (Exception e)
-	 {
-	 System.out.println("error");
-	 }
-	 
-	 /*if(result != 1){ 
-		        return "Invalid"; 
-	     } */
-		    return "successfully updated values"+users; 
-		} 
-@DELETE
-@Path("/delete")
-@Produces(MediaType.APPLICATION_JSON) 
-@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-public String deleet(@FormParam("FirstName") String FirstName,
-		@FormParam("LastName") String LastName,
-		@FormParam("Email") String Email,
-        @FormParam("username") String username,
-        @FormParam("password") String password,
-        @FormParam("confirmpassword") String confirmpassword){
-	
-	User user = new User(FirstName,LastName,Email,username,password,confirmpassword);
-	
-	 userDao.deleteUser(user);
-	
-    String users = null;
-	 try 
-	 {
-	 ArrayList<User> userList = null;
-	 SecurityManager securityManager= new SecurityManager();
-	 userList = securityManager.getAllUsersList();
-	//for (User user3 : userList) {
-		 String EMAIL_PATTERN = 
-				 "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-				 + "+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-				 Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-if( user.getEmail() == null || user.getEmail().trim().equals("")&& !pattern.matcher(Email).matches())
-{
-	//System.out.println("please enter valid email");
-		 
-}
-else{
-	if(user.getEmail().equals(Email))	{			  
-		userList.add(user);
-		Gson gson =new Gson(); 
-	 		 users =gson.toJson(user); 
-			  }
 
-}
-	 
-	 }
-	 catch (Exception e)
-	 {
-	 System.out.println("error");
-	 }
-	 
-	/* if(result != 1){ 
-		         return "Invalid email"; 
-		     } */
-		      return "Successfully Deleted "+users; 
-		 } 
-		 } 
+		@PUT
+		@Path("/put")
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response updateInJSON(User user) throws SQLException {
+			userDao.updateUser(user);
+			ArrayList<User> userList = new ArrayList<User>();
+		 	userList.add(user);
+			String result = "Updated: " + user;
+			return Response.status(201).entity(result).build();
+			
+		}
+		
+		@DELETE
+		@Path("/delete")
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response DeleteInJSON(User user) throws ApplicationException, SQLException {
+			userDao.deleteUser(user);
+			ArrayList<User> userList = new ArrayList<User>();
+		 	userList.add(user);
+			String result = "Deleted: " + user;
+			return Response.status(201).entity(result).build();
+			
+		}
+		
+	}
